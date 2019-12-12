d3d526c2ac44a44ac0cacd4607d2789e7cb10ad8
Renate <renate.gouveia@gmail.com>
2019-12-13 02:13:15 +0800

    Testing library implemented and running successfully

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import {Fetch} from "shared/utils";
 const styles = require("./home.module.scss");
 
-const HomePageContainer = () => {
-  React.useEffect(() => {
-    Fetch.searchMovie("matrix")
-      .then(res => console.log("RES: ", res))
-      .catch(err => console.log("ERROR: ", err));
-  }, []);
-  return <div>Hello</div>;
+interface IProps {
+  children: ReactNode;
+}
+
+const HomePageContainer = ({children}: IProps) => {
+  const [showMessage, setShowMessage] = React.useState(false);
+  return (
+    <div>
+      <input
+        data-testid="test-checkbox"
+        id="toggle"
+        type="checkbox"
+        onChange={e => setShowMessage(e.target.checked)}
+        checked={showMessage}
+      />
+      {showMessage ? children : null}
+    </div>
+  );
 };
 
 export default HomePageContainer;
