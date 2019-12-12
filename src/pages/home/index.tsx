e113bbe98c46cad195771f00e26cb02e7ada0b0c
xXValhallaCoderXx <renate.gouveia@gmail.com>
2019-12-12 14:11:03 +0800

    Setup base application

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const styles = require("./home.module.scss");
+
+
+const HomePage = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default HomePage;
---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {hot} from "react-hot-loader";
+
+import {Route, Router, Switch} from "react-router-dom";
+import {createBrowserHistory} from "history";
+
+import {
+  Home,
+  NotFound
+} from "./pages";
+
+const history = createBrowserHistory();
+
+const Routes = () => {
+  return (
+    <Router history={history}>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+  );
+};
+
+export default hot(module)(Routes);
