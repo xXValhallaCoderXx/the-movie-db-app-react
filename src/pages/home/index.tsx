632806994263deb4ab2b33d85d29deecb1631a57
xXValhallaCoderXx <renate.gouveia@gmail.com>
2019-12-12 16:51:49 +0800

    Api working as expected using env variables

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import {Fetch} from "shared/utils";
 const styles = require("./home.module.scss");
 
-const HomePage = () => {
+const HomePageContainer = () => {
+  React.useEffect(() => {
+    Fetch.searchMovie("matrix")
+      .then(res => console.log("RES: ", res))
+      .catch(err => console.log("ERROR: ", err));
+  }, []);
   return <div>Hello</div>;
 };
 
-export default HomePage;
+export default HomePageContainer;
