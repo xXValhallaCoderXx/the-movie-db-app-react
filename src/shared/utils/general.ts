e93b45718afa6ec5a2943ab1a61141cbfef6f2fb
xXValhallaCoderXx <renate.gouveia@gmail.com>
2019-12-13 13:30:55 +0800

    Mute false positive typescript error

--- src/shared/utils/general.ts
+++ src/shared/utils/general.ts
@@ -2,6 +2,7 @@ export const debounce = (fn: any, time: number) => {
   let timeout: any;
 
   return function() {
+    // @ts-ignore: Mute `this` error
     const functionCall = () => fn.apply(this, arguments);
 
     clearTimeout(timeout);
