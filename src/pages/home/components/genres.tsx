8ff655033b5c6ae7489d13edac452f8afc26df06
xXValhallaCoderXx <renate.gouveia@gmail.com>
2019-12-16 07:02:08 +0800

    Add initial genre mapping to movie detail

--- src/pages/home/components/genres.tsx
+++ src/pages/home/components/genres.tsx
@@ -4,12 +4,35 @@ interface IProps {
   genres: any;
 }
 
+const colourMapper = (genre: string) => {
+  let color;
+  switch (genre.toLowerCase()) {
+    case "adventure":
+      color = "bg-green-500";
+      break;
+
+    case "action":
+      color = "bg-red-500";
+      break;
+
+    case "thriller":
+      color = "bg-orange-500";
+      break;
+
+    default:
+      color = "bg-blue-500";
+  }
+  return color;
+};
+
 const Genres = ({genres}: IProps) => {
   return genres.map((genre: any, index: any) => {
     console.log("GENRES: ", genre);
     return (
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5"
+        className={`${colourMapper(
+          genre.name
+        )} hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5 text-xs`}
         key={index}>
         {genre.name}
       </button>
